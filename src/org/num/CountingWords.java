229ab168ae03b332adf7d39498669ab3bfc7de9b
venkatthiru1122 <venkadesht285@gmail.com>
2022-10-12 07:22:26 +0530

    First

---
+++ src/org/num/CountingWords.java
@@ -0,0 +1,19 @@
+package org.num;
+import java.util.Scanner;
+public class CountingWords {
+public static void main (String [] args) {
+	Scanner s =  new Scanner(System.in);
+	
+	int count = 0;
+	
+	System.out.println("Input : ");
+	String s1 = s.nextLine();
+	
+	String[] word = s1.split("\\s");
+	for(int i=0; i<word.length; i++ ) {
+		
+	count++;		
+	}
+	System.out.println("Total Words in the Sentence : "+count);
+}
+}
